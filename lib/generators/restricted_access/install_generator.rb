92f37846326281b8fb79b8463cdd5fd876a7365d
4nt1 <antoinemary@hotmail.fr>
2014-10-22 14:03:31 +0200

    fix in generator

--- lib/generators/restricted_access/install_generator.rb
+++ lib/generators/restricted_access/install_generator.rb
@@ -3,7 +3,7 @@ module RestrictedAccess
     class InstallGenerator < Rails::Generators::NamedBase
       include Rails::Generators::ResourceHelpers
       source_root File.expand_path('../templates', __FILE__)
-      argument      :levels,            type: :array,   default: ['normal', 'super'],   desc: "List of the differents access levels"
+      argument      :levels,            type: :array,   default: ['normal', 'super'],   desc: "List of the differents access levels", banner: 'level level'
       class_option  :resource_name,     type: :string,  default: 'user'
       class_option  :controller_scope,  type: :string,                                  desc: "Scope of the concerned controllers"
 
